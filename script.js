c8c2c0fac92f043f5f032108622b9f1267e6b9ef
gridsyntax-admin <gridsyntax@gmail.com>
2025-12-26 09:52:44 -0800

    Fixed spelling error on variable.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const resultsNav = document.getElementById('resultsNav');
-const favoritesNav = document.getElementById('ritesNav');
+const favoritesNav = document.getElementById('favoritesNav');
 const imagesContainer = document.querySelector('.images-container');
 const saveConfirmed = document.querySelector('.save-confirmed');
 const loader = document.querySelector('.loader');
